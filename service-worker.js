282daf21346385e8d1466c708a82f1542cd4b087
dhorecreacion <dhorecreacion@gmail.com>
2025-06-04 10:54:45 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+    const CACHE_NAME = "app-cache-v1";
+    const urlsToCache = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/favicon-aprobacion.ico",
+    "/logo-dho.png",
+    ];
+
+    self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+    });
+
+    self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+        })
+    );
+    });
+
+    self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => Promise.all(
+        cacheNames.map(cache => {
+            if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+        ))
+    );
+    });
